939fec78502583147983aebcb3e8435773a83855
Jordan Lamoreaux <jnacious88@gmail.com>
2022-03-29 21:32:00 -0500

    add game over screen

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CharacterInput, GameResult, RESULT } from '../App';
+import { BACKGROUND_COLOR, LETTER_COLOR } from './LetterField';
+import { buttonStyle, DEFAULT_BACKGROUND_COLOR } from './KeyboardButton';
 
 type GameOverProps = {
   gameResult: GameResult;
@@ -14,14 +16,25 @@ type ShareButtonProps = {
 
 const ShareButton = ({ label, shareFunction }: ShareButtonProps) => {
   return (
-    <button onClick={shareFunction}>
+    <button
+      onClick={shareFunction}
+      style={{
+        ...buttonStyle,
+        border: 'none',
+        backgroundColor: DEFAULT_BACKGROUND_COLOR,
+        fontWeight: '600',
+        color: LETTER_COLOR.DEFAULT,
+        fontSize: '1em',
+      }}
+    >
       {label}
     </button>
   )
 }
 
 const GameOver = ({ gameResult, attempts, gameNumber }: GameOverProps) => {
-  const generateBlocks = (attempts: CharacterInput[][]) : string => {
+  const [isClosed, setIsClosed] = useState(false);
+  const generateBlocks = (attempts: CharacterInput[][]): string => {
     const emojiBlocks = attempts.map(row => {
       return row.map(char => {
         const result = char.result;
@@ -36,19 +49,39 @@ const GameOver = ({ gameResult, attempts, gameNumber }: GameOverProps) => {
       }).join('')
     });
     return `Hexle ${gameNumber} ${attempts.length}/6\n${emojiBlocks.join("\r\n")}`;
-  }  
-  const shareToClipboard = () => {
-    console.log('I win!!');
   }
+  
+  const shareToClipboard = (message: string) => {
+    navigator.clipboard.writeText(message)
+  }
+
+  const shareResult = (message: string) => {
+    if (navigator.share) {
+      navigator.share({text: message});
+    }
+  }
+
+  const resultsToShare = generateBlocks(attempts);
+  
+
   if (gameResult !== GameResult.IN_PROGRESS) {
     return (
       <div
         style={{
-
+          position: 'absolute',
+          backgroundColor: BACKGROUND_COLOR.INCORRECT,
+          padding: '10px',
+          margin: 'auto',
+          left: '0',
+          right: '0',
+          top: '150px',
+          maxWidth: '300px'
         }}
       >
-        {generateBlocks(attempts)}
-        <ShareButton label="Share" shareFunction={shareToClipboard} />
+        <button style={{border: 'none', background: 'none', color: LETTER_COLOR.DEFAULT, right: 0, left: 'auto', margin: 'auto'}}>Close</button>
+        <h3>{GameResult.WIN ? "You Win!" : "Game Over"}</h3>
+        <ShareButton label="Share" shareFunction={() => shareResult(resultsToShare)} />
+        <ShareButton label="Copy to Clipboard" shareFunction={() => shareToClipboard(resultsToShare)} />
       </div>
     )
   } else {
