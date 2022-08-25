7389ea780f7d80317b4c93693f25d6b6861e7bc4
Jordan <jnacious88@gmail.com>
2022-08-25 13:29:30 -0500

    add footer

---
+++ src/utils/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export enum BACKGROUND_COLOR {
+  INITIAL = '#193140',
+  INCORRECT = '#030a0e',
+  IN_WORD = '#efac11',
+  CORRECT = '#00c361',
+}
+
+export enum LETTER_COLOR {
+  DEFAULT = '#d7dadc',
+  ALTERNATE = '#111',
+}
+
+export const DEFAULT_BACKGROUND_COLOR = '#767676';
+
+export const Copyright = () => {
+  const date = new Date();
+  return <span>&copy; {date.getFullYear()} </span>;
+};
